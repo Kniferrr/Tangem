4dd973dab4fdc65a57984a1ea9df3c158eb7033d
Dmitry Pankov <pankov.dmitry.dev@gmail.com>
2024-01-11 17:39:39 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import TopBanner from "./TopBanner";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <TopBanner />
+      </div>
+    </>
+  );
+}
+
+export default App;
